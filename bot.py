2f746a6a170356297986bbc204cf03dc03f45a8f
PabloMartinsBorges <51960948+PabloMartinsBorges@users.noreply.github.com>
2024-05-07 18:45:18 -0300

    correção do design do cadastro, mudança de cor e inserção de icon da logo

--- bot.py
+++ bot.py
@@ -1,162 +1,196 @@
-import tkinter as tk
-from tkinter import Tk, Label, Menu, ttk, Scrollbar, Text, Button
-from ttkbootstrap import Style
-from ttkthemes import ThemedStyle
+import tkinter
+import tkinter.messagebox
+import customtkinter
 import difflib
+from datetime import datetime
 from teste import perguntas_respostas
 
-class Chatbot:
-    def __init__(self, janela):
-        self.janela = janela
-        self.janela.title("Núcleo Multidimensional de Gestão do Patrimônio.")
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("orange.json")  # Themes: "blue" (standard), "green", "dark-blue", "orange.json" (ConnectMuseum)
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.iconbitmap('images/logo.ico')
+
         self.perguntas_respostas = perguntas_respostas
 
-        # Adiciona campos em um frame novo
-        self.notebook = ttk.Notebook(janela)
-        self.notebook.pack(fill='both', expand='yes')
+        # configure window
+        self.title("Núcleo Multidimensional de Gestão do Patrimônio.")
+        self.geometry(f"{500}x{300}")  # Aumentando o tamanho da janela de login
 
-        # Adiciona um campo de texo
-        self.conversa = tk.Text(janela, width=50, height=20, font='Arial 10', relief='groove')
-        self.conversa.insert(tk.END,"Bem vindo ao Chatbot do Núcleo Multidimensional de Gestão do Patrimônio!\nTodas as informações sobre o acervo de cultura popular que foram e são pesquisadas estão aqui.\nAtualmente possuimos um acervo com mais de 90 obras de cultura popular localizadas no Rio de Janeiro.\nPara uma melhor experiência tente ser específico quanto as perguntas.\nAproveite!\n\n")
-        self.conversa.configure(state='disabled')
-        self.conversa.pack(fill='both', expand='yes', padx=10, pady=5, side='top')
+        # create login frame with widgets
+        self.login_frame = customtkinter.CTkFrame(self, width=300, corner_radius=0)
+        self.login_frame.pack(expand=True, fill='both')
 
-        scrollbar = ttk.Scrollbar(janela, command=self.conversa.yview)
-        scrollbar.pack(side="right", fill="y")
-        self.conversa.config(yscrollcommand=scrollbar.set)
+        customtkinter.CTkLabel(self.login_frame, text="Login de Usuário", font=customtkinter.CTkFont(size=16, weight="bold")).pack(pady=10)
 
-        # Adiciona um texto indicando onde escrever a mensagem
-        self.label = Label(text="Digite aqui sua mensagem:", font='Arial 15', relief='groove')
-        self.label.pack(pady=10, padx=10)
+        customtkinter.CTkLabel(self.login_frame, text="Nome de Usuário:").pack(pady=5)
+        self.username_entry = customtkinter.CTkEntry(self.login_frame)
+        self.username_entry.pack()
 
-        # wdiget para a entrada do usuário
-        self.mensagem = tk.Entry(janela, width=50, font='Arial 10', relief='groove')
-        self.mensagem.pack(padx=10, pady=10)
-        self.mensagem.bind("<Return>", self.enviar_mensagem)
+        customtkinter.CTkLabel(self.login_frame, text="Senha:").pack(pady=5)
+        self.password_entry = customtkinter.CTkEntry(self.login_frame, show="*")
+        self.password_entry.pack()
 
-        #Menu Chat
-        self.chat_frame = tk.Frame(self.notebook)
-        self.notebook.add(self.chat_frame, text="Chat")
+        # Botão para confirmar o login
+        customtkinter.CTkButton(self.login_frame, text="Login", command=self.verificar_login).pack(pady=10)
 
-        # Adiciona um texo para indicar a conversa do chatbot - CHAT
-        tk.Label(self.chat_frame, text="NUGEP Bot", font="Courier 40").pack(fill='both', expand='yes', padx=10, pady=5, side='top')
-        self.chat_bot = tk.Label(self.chat_frame, font="Arial 10")
-        self.chat_bot.pack()
+        # Botão para cadastrar novo usuário
+        customtkinter.CTkButton(self.login_frame, text="Cadastrar Novo Usuário", command=self.abrir_janela_cadastro).pack(pady=5)
 
-        #Menu Cadastro
-        self.cadastro_frame = tk.Frame(self.notebook)
-        self.notebook.add(self.cadastro_frame, text="Cadastro")
+        # Dicionário para armazenar usuários cadastrados
+        self.usuarios_cadastrados = {"Escobar": "nugepbot"}
 
-        #Botão de cadastrar - CADASTRO
-        self.botao_cadastro = tk.Button(self.cadastro_frame, text="Cadastrar usuário",command=self.abrir_janela_cadastro)
-        self.botao_cadastro.grid(row=0, pady=10)
+    def verificar_login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
 
-        #Menu História
-        self.planilha_frame = tk.Frame(self.notebook)
-        self.notebook.add(self.planilha_frame, text="Sua História")
+        if username in self.usuarios_cadastrados and self.usuarios_cadastrados[username] == password:
+            self.abrir_chatbot(username)
+        else:
+            tkinter.messagebox.showerror("Erro de Login", "Nome de usuário ou senha incorretos.")
 
-        # Adiciona botão para abrir janela de cadastro - HISTÓRIA
-        self.botao_history = tk.Button(self.planilha_frame, text="Cadastrar história",command=self.abrir_janela_history)
-        self.botao_history.grid(row=0, pady=10)
+    def abrir_chatbot(self, username):
+        self.login_frame.destroy()
+        # Código para abrir o chatbot
+        chatbot_window = ChatbotWindow(username)
+        chatbot_window.mainloop()
 
-        #Menu - Informação
-        self.dados_frame = tk.Frame(self.notebook)
-        self.notebook.add(self.dados_frame, text='Informações')
-
-        self.label_dados = tk.Label(self.dados_frame, text='Aqui estão disponibilizadas os relatórios de uso do chatbot', font="Arial 10", relief='raised')
-        self.label_dados.grid(row=0, column=0)
-
-        self.botao_dados = tk.Button(self.dados_frame, text="Gerar relatório", command=self.abrir_gerar_relatorio)
-        self.botao_dados.grid(row=0,column=1,  pady=10)
 
     def abrir_janela_cadastro(self):
-        janela_cadastro = tk.Toplevel(self.janela)
-        janela_cadastro.title("Cadastro de usuário")
-        janela_cadastro.geometry("500x400")
-        janela_cadastro.resizable(True, True)
+        cadastro_frame = customtkinter.CTkToplevel(self)
+        cadastro_frame.title("Cadastro de usuário")
+        cadastro_frame.geometry(f"{500}x{300}")
 
-        tk.Label(janela_cadastro, text="Nome:").grid(row=1, column=0)
-        self.nome_entry = tk.Entry(janela_cadastro)
-        self.nome_entry.grid(row=1, column=1)
+        cadastro_frame = customtkinter.CTkFrame(cadastro_frame, width=300, corner_radius=0)
+        cadastro_frame.pack(expand=True, fill='both')
+        
 
-        tk.Label(janela_cadastro, text="Idade:").grid(row=2, column=0)
-        self.idade_entry = tk.Entry(janela_cadastro)
-        self.idade_entry.grid(row=2, column=1)
+        customtkinter.CTkLabel(cadastro_frame, text="Nome de Usuário:").pack(pady=5)
+        self.novo_username_entry = customtkinter.CTkEntry(cadastro_frame)
+        self.novo_username_entry.pack()
 
-        tk.Label(janela_cadastro, text="Email:").grid(row=3, column=0)
-        self.email_entry = tk.Entry(janela_cadastro)
-        self.email_entry.grid(row=3, column=1)
+        customtkinter.CTkLabel(cadastro_frame, text="Senha:").pack(pady=5)
+        self.nova_password_entry = customtkinter.CTkEntry(cadastro_frame, show="*")
+        self.nova_password_entry.pack()
 
-        tk.Label(janela_cadastro, height=0, text="Endereço:").grid(row=4, column=0)
-        self.endereco_entry = tk.Entry(janela_cadastro)
-        self.endereco_entry.grid(row=4, column=1)
-
-        botao_salvar_cadastro = ttk.Button(janela_cadastro, text="Salvar", command=self.salvar_usuario)
-        botao_salvar_cadastro.grid(row=5, column=3, pady=10)
-
-        self.conversa_cadastro_label = tk.Label(janela_cadastro, text='Janela de Usuários:', font="Arial 20", relief='groove')
-        self.conversa_cadastro_label.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
-
-        self.conversa_cadastro = tk.Text(janela_cadastro, width=30, height=10, font='Arial 10', relief='groove')
-        self.conversa_cadastro.configure(state='disabled')
-        self.conversa_cadastro.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
-
-
-    def abrir_gerar_relatorio(self):
-        janela_relatorio = tk.Toplevel(self.janela)
-        janela_relatorio.title("Relatórios de uso")
-        janela_relatorio.geometry("400x450")
-        janela_relatorio.resizable(True, True)
-
-        self.label_relatorio = tk.Text(janela_relatorio, font='Verdana 10')
-        self.label_relatorio.pack()
-
-        botao_relatorio = tk.Button(janela_relatorio, text="Dashboard",font="Verdana 10")
-        botao_relatorio.pack(pady=15)
-        #FALTA UMA FUNÇÃO PARA MOSTRAR OS DADOS - CADASTRO, HISTÓRIA E CONVERSAS
-
-    def abrir_janela_history(self):
-        janela_history = tk.Toplevel(self.janela)
-        janela_history.title("Cadastro de história")
-        janela_history.geometry("400x400")
-        janela_history.resizable(True, True)
-
-        tk.Label(janela_history, text="Conte aqui:").pack()
-        self.history_text = tk.Entry(janela_history, font='Arial 10', relief='groove')
-        self.history_text.pack()
-
-        self.history_cadastro = tk.Text(janela_history, width=50, height=10, pady=20, padx=20, font='Arial 10', relief='groove')
-        self.history_cadastro.configure(state='disabled')
-        self.history_cadastro.pack(pady=15)
-
-        botao_salvar_history = tk.Button(janela_history, text="Salvar", command=self.salvar_history,font="Verdana 10")
-        botao_salvar_history.pack(pady=15)
-
-    def salvar_history(self):
-        self.history_cadastro.configure(state='normal')
-        history = self.history_text.get()
-        self.history_cadastro.insert(tk.END, f'História Cadastrada com Sucesso.\nRelato: {history}\n')
-        self.history_cadastro.configure(state='disabled')
+        customtkinter.CTkButton(cadastro_frame, text="Salvar", command=self.salvar_usuario).pack(pady=10)
 
     def salvar_usuario(self):
-        self.conversa_cadastro.configure(state='normal')
-        nome = self.nome_entry.get()
-        idade = self.idade_entry.get()
-        email = self.email_entry.get()
-        endereco = self.endereco_entry.get()
+        novo_username = self.novo_username_entry.get()
+        nova_password = self.nova_password_entry.get()
 
-        self.conversa_cadastro.insert(tk.END,f'Cadastrado com sucesso: Nome {nome},' f'Idade: {idade},' f'Email: {email},' f'Endereço: {endereco}')
-        self.conversa_cadastro.configure(state='disabled')
+        # Adiciona o novo usuário ao dicionário de usuários cadastrados
+        self.usuarios_cadastrados[novo_username] = nova_password
+
+        tkinter.messagebox.showinfo("Cadastro de Usuário", "Usuário cadastrado com sucesso!")
+
+    def usernames_cadastrados(self):
+        return self.usuarios_cadastrados.keys()
+    
+
+class ChatbotWindow(customtkinter.CTk):
+    def __init__(self, username):
+        super().__init__()
+        self.iconbitmap('images/logo.ico')
+
+        self.perguntas_respostas = perguntas_respostas
+
+        # configure window
+        self.title("Chatbot - Núcleo Multidimensional de Gestão do Patrimônio.")
+        self.geometry(f"{1100}x{580}")
+
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Nugep\n Bot", font=customtkinter.CTkFont(size=40, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 30))
+
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.abrir_janela_cadastro)
+        self.sidebar_button_4.grid(row=6, column=0, padx=20, pady=10)
+       
+        # create main entry and button
+        self.mensagem = customtkinter.CTkEntry(self, placeholder_text="Insira o sua mensagem")
+        self.mensagem.grid(row=3, column=1, columnspan=2, rowspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        self.mensagem.bind("<Return>", self.enviar_mensagem)
+
+        # create textbox
+        self.conversa = customtkinter.CTkTextbox(self, width=250)
+        self.conversa.grid(row=0, column=1, rowspan=3, padx=(20, 0), pady=(5, 0), sticky="nsew")
+            
+        self.sidebar_frame_dir = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame_dir.grid(row=0, column=3, padx =(20,0), rowspan=4, sticky="nsew")
+        self.sidebar_frame_dir.grid_rowconfigure(4, weight=1)
+        
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame_dir, text="Tema:", anchor="w")
+        self.appearance_mode_label.grid(row=0, column=3, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame_dir, values=["Dark", "Light", "System"],
+                                                                       command=self.change_appearance_mode_event)
+
+
+        self.appearance_mode_optionemenu.grid(row=1, column=3, padx=20, pady=(10, 10))
+
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame_dir, text="Escala da Tela:", anchor="w")
+        self.scaling_label.grid(row=2, column=3, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame_dir, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=3, column=3, padx=20, pady=(10, 20))
+
+        # set default values
+        self.sidebar_button_1.configure(text="Informações")
+        self.sidebar_button_2.configure(text="Gerar Relatório")
+        self.sidebar_button_3.configure(text="Feedbacks")
+    
+        self.conversa.insert("0.0", "Bem-vindo ao Chatbot do Núcleo Multidimensional de Gestão do Patrimônio!\n"
+                                     "Todas as informações sobre o acervo de cultura popular que foram e são pesquisadas estão aqui.\n"
+                                     "Atualmente possuímos um acervo com mais de 90 obras de cultura popular localizadas no Rio de Janeiro.\n"
+                                     "Para uma melhor experiência, tente ser específico quanto às perguntas.\nAproveite!\n\n")
+        
+    #tesat se todos os butões estão funcionando
+    def open_input_dialog_event(self):
+        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
+        print("CTkInputDialog:", dialog.get_input())
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+    def sidebar_button_event(self):
+        print("sidebar_button click")
 
     def enviar_mensagem(self, event):
         self.conversa.configure(state='normal')
         mensagem_usuario = self.mensagem.get()
 
-        self.mensagem.delete(0, tk.END)
+        self.mensagem.delete(0, customtkinter.END)
         resposta_chatbot = self.processar_mensagem(mensagem_usuario)
 
-        self.conversa.insert(tk.END, "Usuário: " + mensagem_usuario + "\n")
-        self.conversa.insert(tk.END, "Chatbot: " + resposta_chatbot + "\n")
+        self.conversa.insert(customtkinter.END, "Usuário: " + mensagem_usuario + "\n")
+    
+        self.conversa.insert(customtkinter.END, "Chatbot: " + resposta_chatbot + "\n")
+        self.conversa.see(customtkinter.END)
         self.conversa.configure(state='disabled')
 
     def processar_mensagem(self, mensagem):
@@ -180,10 +214,34 @@ class Chatbot:
         # Caso não encontre nenhuma correspondência, retorna uma mensagem padrão
         return "Desculpe, não entendi a pergunta. Tente novamente ou reformule a sua pergunta."
 
-estilo = Style(theme="darkly")
-janela = estilo.master
-janela.title("Núcleo Multidimensional de Gestão do Patrimônio.")
-janela.geometry('750x600')
-chatbot = Chatbot(janela)
+    def abrir_janela_cadastro(self):
+        cadastro_frame = customtkinter.CTkToplevel(self)
+        cadastro_frame.title("Cadastro de usuário")
+        cadastro_frame.geometry(f"{500}x{300}")
 
-janela.mainloop()
+        cadastro_frame = customtkinter.CTkFrame(cadastro_frame, width=300, corner_radius=0)
+        cadastro_frame.pack(expand=True, fill='both')
+        
+
+        customtkinter.CTkLabel(cadastro_frame, text="Nome de Usuário:").pack(pady=5)
+        self.novo_username_entry = customtkinter.CTkEntry(cadastro_frame)
+        self.novo_username_entry.pack()
+
+        customtkinter.CTkLabel(cadastro_frame, text="Senha:").pack(pady=5)
+        self.nova_password_entry = customtkinter.CTkEntry(cadastro_frame, show="*")
+        self.nova_password_entry.pack()
+
+        customtkinter.CTkButton(cadastro_frame, text="Salvar", command=self.salvar_usuario).pack(pady=10)
+
+    def salvar_usuario(self):
+        novo_username = self.novo_username_entry.get()
+        nova_password = self.nova_password_entry.get()
+
+        # Adiciona o novo usuário ao dicionário de usuários cadastrados
+        self.usuarios_cadastrados[novo_username] = nova_password
+
+        tkinter.messagebox.showinfo("Cadastro de Usuário", "Usuário cadastrado com sucesso!")
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
